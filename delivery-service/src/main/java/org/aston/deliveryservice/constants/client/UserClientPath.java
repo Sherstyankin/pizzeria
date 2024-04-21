dcd1f4c9216b8b38508a80576b2f4f7ef57a7c28
Sergey Sherstyankin <sherstyankinsergei.gmail.com>
2024-04-21 09:24:44 +0300

    refactor: add connection properties

--- delivery-service/src/main/java/org/aston/deliveryservice/constants/client/UserClientPath.java
+++ delivery-service/src/main/java/org/aston/deliveryservice/constants/client/UserClientPath.java
@@ -1,5 +1,5 @@
 package org.aston.deliveryservice.constants.client;
 
 public class UserClientPath {
-    public static final String POST = "http://127.0.0.1:8080/user/sendPizza";
+    public static final String POST = "http://127.0.0.1:9096/api/pizzas";
 }
